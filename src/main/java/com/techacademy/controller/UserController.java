11181a7f2e55eeebf557f87a902a196a98636bdc
JumpeiMatsuoka <totoro84@hotmail.co.jp>
2024-01-21 07:00:57 +0900

    Chapter8終了

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -3,7 +3,12 @@ package com.techacademy.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.techacademy.entity.User;
 import com.techacademy.service.UserService;
 
 @Controller
@@ -22,5 +27,27 @@ public class UserController {
         model.addAttribute("userlist", service.getUserList());
         // user/list.htmlに画面遷移
         return "user/list";
+    }
+    @GetMapping("/register")
+    public String getRegister(@ModelAttribute User user) {
+        return "user/register";
+    }
+    @PostMapping("/register")
+    public String postRegister(User user) {
+
+        service.saveUser(user);
+        return "redirect://user/list";
+    }
+    @GetMapping("/update/{id}/")
+    public String getUser(@PathVariable("id")Integer id,Model model) {
+
+        model.addAttribute("user",service.getUser(id));
+        return "user/update";
+    }
+
+    @PostMapping("/update/{id}/")
+    public String postUser(User user) {
+        service.saveUser(user);
+        return"redirect:/user/list";
     }
 }
