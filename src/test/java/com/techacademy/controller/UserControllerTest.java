c28b84ad883311bc459be64fb2f2c54652098c2a
JumpeiMatsuoka <totoro84@hotmail.co.jp>
2024-02-02 21:33:54 +0900

    3回目修正

--- src/test/java/com/techacademy/controller/UserControllerTest.java
+++ src/test/java/com/techacademy/controller/UserControllerTest.java
@@ -1,11 +1,5 @@
 package com.techacademy.controller;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,10 +13,21 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
-class UserControllerTest {
+public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -31,17 +36,39 @@ class UserControllerTest {
     private WebApplicationContext webApplicationContext;
 
     @BeforeEach
-    void beforeEach() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).apply(springSecurity()).build();
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                    .webAppContextSetup(webApplicationContext)
+                    .apply(springSecurity())
+                    .build();
     }
 
     @Test
     @DisplayName("Userリストページの検証")
     @WithMockUser
-    void testGetList() throws Exception {
+    public void testGetUserList() throws Exception {
         mockMvc.perform(get("/user/list"))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("userlist"))
+                .andExpect(model().attribute("userlist", hasSize(3)))
+                .andExpect(model().attribute("userlist", hasItem(
+                    allOf(
+                        hasProperty("id", is(1)),
+                        hasProperty("name", is("キラメキ太郎"))
+                    )
+                )))
+                .andExpect(model().attribute("userlist", hasItem(
+                    allOf(
+                        hasProperty("id", is(2)),
+                        hasProperty("name", is("キラメキ次郎"))
+                    )
+                )))
+                .andExpect(model().attribute("userlist", hasItem(
+                    allOf(
+                        hasProperty("id", is(3)),
+                        hasProperty("name", is("キラメキ花子"))
+                    )
+                )))
                 .andExpect(view().name("user/list"));
     }
 }
